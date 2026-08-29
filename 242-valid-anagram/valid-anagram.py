c6a7b94e77b05d713b3c349de19a8603ca3c4199
Uday D Mahan <udaymahan098@gmail.com>
2026-08-29 21:17:55 +0530

    Time: 7 ms (91.58%) | Memory: 19.4 MB (46.02%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq1 = Counter(s)
+        freq2 = Counter(t)
+        
+        if freq1 == freq2:
+            return True
+
+        else:
+            return False
